c60f89ca3957c69a82c15aa4376108ff58fcdce7
jotamaster <j3anhernandez@gmail.com>
2017-05-25 22:09:48 -0400

    cambio de titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Examen Uno Enhorabuena
                 </div>
 
                 <div class="links">
